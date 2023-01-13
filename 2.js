1ae269280caa5f0f1cd5844fcf448f3465982d5a
vidhi000 <vidhi5410rana@gmail.com>
2023-01-13 16:42:58 +0530

    changes

--- 2.js
+++ 2.js
@@ -1,3 +1,9 @@
-let inputString = 'col1,col2\na,b\nc,d'
+//convert string to 2D array 
+let data = 'col1,col2\na,b\nc,d'
 //result --> [{'col1': 'a', 'col2': 'b'}, {'col1': 'c', 'col2': 'd'}]
 
+conv =(data,del = ',')=>{
+  const con = data.slice(0,data.indexOf('\n')).split(',')
+  return data
+}
+console.log(data);
