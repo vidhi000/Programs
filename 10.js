dd998673153c3b31c53718ff7fcaae4286857a94
vidhi000 <vidhi5410rana@gmail.com>
2023-01-13 15:46:08 +0530

    changes

--- 10.js
+++ 10.js
@@ -1,3 +1,15 @@
-// Write a program to convert time 24 hours to 12 hours.
-input1 = 0
-input2 = 13
+let input = [10,0,23,18,15,20]
+
+input.forEach((number)=>{
+let val ;
+if(number === 0  || number === 12)
+    val = `${number} : 12 ${number === 0? "AM" : "PM"}`
+else{
+    val = `${number} : ${number%12} ${number < 12 ? "AM" : "PM"}`
+}
+console.log(val);
+
+})
+
+
+
