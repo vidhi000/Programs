a8025ba6280ca0b1b9a6ea6b77a0fa4c2826fd1e
Vidhi_Rana <80507737+vidhi000@users.noreply.github.com>
2023-01-16 10:53:01 +0530

    Update 4.js

--- 4.js
+++ 4.js
@@ -8,4 +8,24 @@ const res = (arr)=>{
 }
 console.log(res([1,2]));
 
+//new
+let arr = [1,2]
+//result --> [[],[1],[2],[2,1]]
+
+const res = (arr)=>{
+   return arr.reduce((a,b)=>
+    a.concat(a.map(item => [b].concat(item))),[[]])
+}
+// console.log([b].concat(item));
+
+console.log(res([1,2]));
+
+//[[]] --> arr with empty sub-arr
+//a = []
+//[b] = 1
+//a.map(item => [b].concat(item)) // [[],[1]] or [1]
+//a.concat([1],[[]]) // [[],[1]]
+
+
+
 
